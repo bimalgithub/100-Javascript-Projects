473bf62dca54ea383967734b884b11569de70a8c
Bimal Lamichhane <linkbimal123@gmail.com>
2020-08-04 21:57:28 +0545

    first draft of binary-to-decimal

---
+++ binary-to-decimal/main.js
@@ -0,0 +1,22 @@
+function convert(){
+    const binary = document.getElementById("b-input").value;
+    console.log("The binary value is : "+ binary);
+    if (binary === '')
+        return 0;
+    binary.split('').map((char) => {
+        if (char !== '0' && char !== '1' ){
+            return error("Please enter valid binary number");
+        }
+    });
+
+    const decimal = parseInt(binary, 2);
+    document.getElementById('d-output').value = decimal;
+    return true;
+}
+
+function clearInput(){
+    console.log("clear clicked");
+    document.getElementById("b-input").value = '';
+    document.getElementById('d-output').value = '';
+    return true;
+}
